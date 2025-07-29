57a1902e1225e73a7fd46c23e18ec5bbc988cdc0
minhphuongliam <duongminhphuong.liam@outlook.com>
2025-07-29 17:29:56 +0700

    build : add dependancies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,14 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    //Kapt
+    id("org.jetbrains.kotlin.kapt")
+    //Google services
+    id("com.google.gms.google-services")
+    //Nav safe args
+    id("androidx.navigation.safeargs.kotlin")
+    //Hilt
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -26,6 +34,9 @@ android {
             )
         }
     }
+    buildFeatures {
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -36,6 +47,49 @@ android {
 }
 
 dependencies {
+    // Navigation Component
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    // Lifecycle ViewModel + Livedata
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+
+    // KTX Activity, Fragment
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.fragment.ktx)
+
+    // Firebase Authentication, Firestore (using BOM for management)
+    // You must apply the platform first.
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.auth.ktx)
+    implementation(libs.firebase.firestore.ktx)
+    implementation(libs.firebase.analytics)
+
+    // Retrofit2, Gson, OkHttp3
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.okhttp.logging.interceptor)
+
+    // Glide
+    implementation(libs.glide)
+    kapt(libs.glide.compiler)
+
+    // Coroutine (ViewModel, retrofit suspend )
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Jetpack Room
+    implementation(libs.androidx.room.runtime)
+    kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+    // Unit test
+    testImplementation(libs.junit)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
